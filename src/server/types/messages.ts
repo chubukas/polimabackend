fbadc0557d62a47dd8f801d62761da57a8953c5a
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2022-03-07 16:53:58 +0100

    created status, context and response message types

---
+++ src/server/types/messages.ts
@@ -0,0 +1,44 @@
+const AUTHORIZATION = 'No Authorization header';
+
+const AUTHENTICATION = 'You must login to perform this operation';
+
+const NO_USER = 'User not found.';
+
+const SERVER_ERROR = 'Something went wrong.';
+
+const INVALID_ROUTE = 'Invalid api route';
+
+const ACCOUNT_INACTIVE = 'User account must be verified.';
+
+const IN_VALID_LOGIN = 'Invalid email or password.';
+
+const UNAUTHORIZED = 'User not authorized to perform action.';
+
+const USER_EXIST = 'User already exist';
+
+const USERNAME_EXIST = 'Username already exist';
+
+const UPDATE_SUCCESS = 'Successfully updated';
+
+const DELETED_SUCCESS = 'Successfully deleted';
+
+const NOT_FOUND = 'Not found';
+
+const STATUS_NOT_AUTHORIZED = 'You are not authorized to login... Please contact admin for details';
+
+export {
+    AUTHORIZATION,
+    AUTHENTICATION,
+    NO_USER,
+    INVALID_ROUTE,
+    SERVER_ERROR,
+    IN_VALID_LOGIN,
+    ACCOUNT_INACTIVE,
+    UNAUTHORIZED,
+    USER_EXIST,
+    USERNAME_EXIST,
+    UPDATE_SUCCESS,
+    DELETED_SUCCESS,
+    NOT_FOUND,
+    STATUS_NOT_AUTHORIZED
+};
